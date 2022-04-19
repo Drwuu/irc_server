94114b2b9f51ea07aab9c2a545363aa6a7f74c80
El Guill <71549232+Spidfail@users.noreply.github.com>
2022-04-19 14:51:24 +0200

    [Events - Implementation] Handle patch
    Added :
    	- Proxy_queue -> added Proxy reference to be able to modify Proxy
    	- Server_queue -> added Server reference to be able to modify Server

--- sources/proxy/Event.cpp
+++ sources/proxy/Event.cpp
@@ -6,7 +6,7 @@
 /*   By: lwourms <lwourms@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/15 16:40:50 by guhernan          #+#    #+#             */
-/*   Updated: 2022/04/17 16:40:12 by lwourms          ###   ########.fr       */
+/*   Updated: 2022/04/19 14:49:18 by guhernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,32 +21,36 @@ Socket_event::~Socket_event() { }
 //
 Proxy_queue::Write::Write() { }
 Proxy_queue::Write::~Write() { }
-void			Proxy_queue::Write::handle() {
-}
+void			Proxy_queue::Write::handle() { }
+void			Proxy_queue::Write::handle(Proxy &) { }
 
 ///////////////////////////////////////////////////////////////////////////////
 //
 Proxy_queue::Write_priority::Write_priority() { }
 Proxy_queue::Write_priority::~Write_priority() { }
 void			Proxy_queue::Write_priority::handle() { }
+void			Proxy_queue::Write_priority::handle(Proxy &) { }
 
 ///////////////////////////////////////////////////////////////////////////////
 //
 Proxy_queue::Confirm_connexion::Confirm_connexion() { }
 Proxy_queue::Confirm_connexion::~Confirm_connexion() { }
 void			Proxy_queue::Confirm_connexion::handle() { }
+void			Proxy_queue::Confirm_connexion::handle(Proxy &) { }
 
 ///////////////////////////////////////////////////////////////////////////////
 //
 Proxy_queue::Refuse_connexion::Refuse_connexion() { }
 Proxy_queue::Refuse_connexion::~Refuse_connexion() { }
 void			Proxy_queue::Refuse_connexion::handle() { }
+void			Proxy_queue::Refuse_connexion::handle(Proxy &) { }
 
 ///////////////////////////////////////////////////////////////////////////////
 //
 Proxy_queue::Disconnect_all::Disconnect_all() { }
 Proxy_queue::Disconnect_all::~Disconnect_all() { }
 void			Proxy_queue::Disconnect_all::handle() { }
+void			Proxy_queue::Disconnect_all::handle(Proxy &) { }
 
 ///////////////////////////////////////////////////////////////////////////////
 //
@@ -54,7 +58,8 @@ Proxy_queue::Disconnect::Disconnect() { }
 Proxy_queue::Disconnect::Disconnect(const fd_type &socket_id)
 	:_socket_id(socket_id) { }
 Proxy_queue::Disconnect::~Disconnect() { }
-void			Proxy_queue::Disconnect::handle() {
+void			Proxy_queue::Disconnect::handle() { }
+void			Proxy_queue::Disconnect::handle(Proxy &) {
 	std::clog << " ------------------- DISCONNECT RECEIVED " << std::endl;
 	std::clog << " ---------- fd = " << _socket_id << std::endl;
 }
@@ -64,6 +69,7 @@ void			Proxy_queue::Disconnect::handle() {
 Proxy_queue::Signal::Signal() { }
 Proxy_queue::Signal::~Signal() { }
 void			Proxy_queue::Signal::handle() { }
+void			Proxy_queue::Signal::handle(Proxy &) { }
 
 ///////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
@@ -80,7 +86,9 @@ Server_queue::Message::Message(Server_queue::Message::data_type data,
 	std::clog << " data = " << _data << " socket = " << _socket->get_fd() << std::endl;
 }
 
-void			Server_queue::Message::handle() {
+void			Server_queue::Message::handle() { }
+
+void			Server_queue::Message::handle(Server &server) {
 	std::clog << " ------------------- MESSAGE RECEIVED " << std::endl;
 	std::clog << " data = " << _data << " socket = " << _socket->get_fd() << std::endl;
 }
@@ -94,7 +102,9 @@ Server_queue::Message_priority::Message_priority() {
 
 Server_queue::Message_priority::~Message_priority() { }
 
-void			Server_queue::Message_priority::handle() {
+void			Server_queue::Message_priority::handle() { }
+
+void			Server_queue::Message_priority::handle(Server &server) {
 	std::clog << " ------------------- HANDLING PRIORITY MESSAGE" << std::endl;
 	std::clog << " data = " << _data << std::endl;
 	std::clog << " socket = " << _socket->get_fd() << std::endl;
@@ -113,9 +123,13 @@ Server_queue::Request_connexion::Request_connexion(const socket_type *client)
 
 Server_queue::Request_connexion::~Request_connexion(){ }
 
-void			Server_queue::Request_connexion::handle(){
+void			Server_queue::Request_connexion::handle() { }
+
+void			Server_queue::Request_connexion::handle(Server &server) {
 	std::clog << " ------------------- HANDLING CONNEXION REQUEST "
 		<< " socket = " << _socket->get_fd() << std::endl;
+	// Call parser
+	// Call client
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -130,7 +144,9 @@ Server_queue::Client_disconnected::Client_disconnected(const socket_type *client
 }
 Server_queue::Client_disconnected::~Client_disconnected(){ }
 
-void			Server_queue::Client_disconnected::handle(){
+void			Server_queue::Client_disconnected::handle() { }
+
+void			Server_queue::Client_disconnected::handle(Server &server) {
 	std::clog << " ------------------- HANDLING DISCONNECTION "
 		<< " socket = " << _socket->get_fd() << std::endl;
 }
@@ -144,7 +160,10 @@ Server_queue::Error::Error(){
 	std::clog << " data = " << _data << std::endl;
 }
 Server_queue::Error::~Error(){ }
-void			Server_queue::Error::handle(){
+
+void			Server_queue::Error::handle(){ }
+
+void			Server_queue::Error::handle(Server &server){
 	std::clog << " ------------------- HANDLING ERROR "
 		<< " socket = " << _socket->get_fd() << std::endl;
 	std::clog << " data = " << _data << std::endl;
@@ -159,7 +178,10 @@ Server_queue::Signal::Signal(){
 	std::clog << " data = " << _data << std::endl;
 }
 Server_queue::Signal::~Signal(){ }
-void			Server_queue::Signal::handle(){
+
+void			Server_queue::Signal::handle(){ }
+
+void			Server_queue::Signal::handle(Server &server){
 	std::clog << " ------------------- HANDLING SIGNAL "
 		<< " socket = " << _socket->get_fd() << std::endl;
 	std::clog << " data = " << _data << std::endl;
