81932b668b9b0d3a035891f071e743c824cf678e
El Guill <71549232+Spidfail@users.noreply.github.com>
2022-04-27 19:22:18 +0200

    Quit patch

--- sources/commands/Quit.cpp
+++ sources/commands/Quit.cpp
@@ -11,6 +11,7 @@ namespace irc {
 			user.disconnect_user(_args[1]);
 		else
 			user.disconnect_user(user.get_nickname());
+		_server->get_event_list().push_back(new Proxy_queue::Disconnect(user.get_socket()->get_fd()));
 	}
 	bool Quit::is_valid_args(User const &) {
 		return true;
